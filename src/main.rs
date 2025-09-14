c496c4700f45ea80e3f9aa74a3f8188ea99f4547
olamilekan@root <olorunnisholaolamilekan@gmail.com>
2025-09-14 19:57:00 +0100

    chore: test solution

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 #![allow(unused_imports)]
 use std::net::TcpListener;
-use std::io::Write;
+use std::io::{Write, Read};
 
 fn main() {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
@@ -11,14 +11,26 @@ fn main() {
     let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
     
     for stream in listener.incoming() {
+        let mut buffer = [0; 1024]; // A buffer to store incoming data   
+
         match stream {
             Ok(mut stream_result) => {
                 println!("accepted new connection");
                 loop {
+                    let data = stream_result.read(&mut buffer);
                     println!("attempting to respond");
-                    match stream_result.write_all(b"+PONG\r\n") {
-                        Ok(_) => println!("responded successfully"),
-                        Err(_) => println!("response failed")
+                    match data{
+                        Ok (0) => {
+                            break;
+                        }
+                        Ok(_) => {
+                            println!("responded successfully");
+                            stream_result.write_all(b"+PONG\r\n").unwrap();
+                        },
+                        Err(_) => {
+                            println!("response failed");
+                            break;
+                        }
                     }
                 }
             }
