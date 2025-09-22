5c00108b7e8459472dd8573b6784422d1f93fda1
olamilekan@root <olorunnisholaolamilekan@gmail.com>
2025-09-22 10:06:02 +0100

    feat: extract response types into sepearate he helper functions

--- src/response.rs
+++ src/response.rs
@@ -4,7 +4,7 @@ use super::{handler::StoreItem, redis_serialize, RedisData};
 
 pub fn respond(req: RedisData, store: &mut HashMap<String, StoreItem>) -> String {
     if req.parses_to_string(Some("ping")) {
-        return String::from("+PONG\r\n");
+        return handle_ping();
     }
 
     if req.is_arr() {
@@ -15,73 +15,14 @@ pub fn respond(req: RedisData, store: &mut HashMap<String, StoreItem>) -> String
                 let cmd = &data[0];
 
                 if cmd.parses_to_string(Some("ping")) {
-                    return String::from("+PONG\r\n");
+                    return handle_ping();
                 } else if data.len() >= 2 {
                     if cmd.is_bulk_string(Some("echo")) {
-                        return redis_serialize(&data[1]);
+                        return handle_echo(data);
                     } else if cmd.is_bulk_string(Some("set")) {
-                        let key = data[1].as_string().unwrap_or(String::from(""));
-                        let value = data[2].as_string().unwrap_or(String::from(""));
-
-                        let can_expire = data.len() >= 5 && data[3].parses_to_string(Some("px"));
-
-                        if can_expire {
-                            let expiry = data[4].as_string().unwrap_or(String::from("0"));
-
-                            store.insert(
-                                key,
-                                StoreItem {
-                                    value,
-                                    created_at: Some(SystemTime::now()),
-                                    expires_after: Some(expiry),
-                                },
-                            );
-                        } else {
-                            store.insert(
-                                key,
-                                StoreItem {
-                                    value,
-                                    created_at: None,
-                                    expires_after: None,
-                                },
-                            );
-                        };
-
-                        return String::from("+OK\r\n");
+                        return handle_set(data, store);
                     } else if cmd.is_bulk_string(Some("get")) {
-                        let key = data[1].as_string().unwrap_or(String::from("empty"));
-                        let value = store.get(&key);
-                        
-                        let data = match value {
-                            Some(val) => {
-                                let StoreItem {
-                                    value,
-                                    created_at,
-                                    expires_after,
-                                } = val;
-
-                                if let Some(expired_at) = expires_after {
-                                    match created_at.unwrap().elapsed() {
-                                        Ok(time) => {
-                                            if time.as_millis()
-                                                >= expired_at.parse::<u128>().unwrap()
-                                            {
-                                                RedisData::NullBulkString(())
-                                            } else {
-                                                RedisData::BulkString(value.to_string())
-                                            }
-                                        }
-                                        _ => RedisData::NullBulkString(()),
-                                    }
-                                } else {
-                                    RedisData::BulkString(value.to_string())
-                                }
-                            }
-
-                            None => RedisData::NullBulkString(()),
-                        };
-
-                        return redis_serialize(&data);
+                        return handle_get(data, store);
                     }
                 }
             }
@@ -93,4 +34,85 @@ pub fn respond(req: RedisData, store: &mut HashMap<String, StoreItem>) -> String
     return redis_serialize(&RedisData::Error(String::from(
         "Error responding to request",
     )));
+}
+
+/* Helpers */
+
+fn handle_ping() -> String {
+    String::from("+PONG\r\n")
+}
+
+fn handle_echo(data: &Vec<RedisData>) -> String {
+    redis_serialize(&data[1])
+}
+
+fn handle_set(data: &Vec<RedisData>, store: &mut HashMap<String, StoreItem>) -> String {
+    if data.len() < 3 {
+        return redis_serialize(&RedisData::Error(String::from(
+            "Error setting key value pair",
+        )));
+    }
+
+    let key = data[1].as_string().unwrap_or(String::from(""));
+    let value = data[2].as_string().unwrap_or(String::from(""));
+
+    let can_expire = data.len() >= 5 && data[3].parses_to_string(Some("px"));
+
+    if can_expire {
+        let expiry = data[4].as_string().unwrap_or(String::from("0"));
+
+        store.insert(
+            key,
+            StoreItem {
+                value,
+                created_at: Some(SystemTime::now()),
+                expires_after: Some(expiry),
+            },
+        );
+    } else {
+        store.insert(
+            key,
+            StoreItem {
+                value,
+                created_at: None,
+                expires_after: None,
+            },
+        );
+    };
+
+    return String::from("+OK\r\n");
+}
+
+fn handle_get(data: &Vec<RedisData>, store: &mut HashMap<String, StoreItem>) -> String {
+    let key = data[1].as_string().unwrap_or(String::from("empty"));
+    let value = store.get(&key);
+
+    let data = match value {
+        Some(val) => {
+            let StoreItem {
+                value,
+                created_at,
+                expires_after,
+            } = val;
+
+            if let Some(expired_at) = expires_after {
+                match created_at.unwrap().elapsed() {
+                    Ok(time) => {
+                        if time.as_millis() >= expired_at.parse::<u128>().unwrap() {
+                            RedisData::NullBulkString(())
+                        } else {
+                            RedisData::BulkString(value.to_string())
+                        }
+                    }
+                    _ => RedisData::NullBulkString(()),
+                }
+            } else {
+                RedisData::BulkString(value.to_string())
+            }
+        }
+
+        None => RedisData::NullBulkString(()),
+    };
+
+    return redis_serialize(&data);
 }
